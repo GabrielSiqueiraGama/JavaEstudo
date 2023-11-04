469d4119b9463f7d242f70ad4244b93c3db0026a
Gabriel Siqueira Gama <siqueiragamagabriel@gmail.com>
2023-11-04 08:06:56 -0300

    Metodos

--- retorno/src/orientacaoObjeto/Produto.java
+++ retorno/src/orientacaoObjeto/Produto.java
@@ -4,4 +4,8 @@ public class Produto {
 	String nome;
 	double preco;
 	double desconto;
+	
+	double precoComDesconto(double desconto) {
+		return preco * (1 - (desconto/100));
+	}
 }
--- retorno/src/orientacaoObjeto/TesteProduto.java
+++ retorno/src/orientacaoObjeto/TesteProduto.java
@@ -17,7 +17,7 @@ public class TesteProduto {
 		p2.desconto = scn.nextDouble();
 		
 		double precofinal1 = p1.preco * (1 -(p1.desconto/100));
-		double precofinal2 = p2.preco * (1 -(p2.desconto/100));
+		double precofinal2 = p2.precoComDesconto(p2.desconto);
 		
 		System.out.println("O preço do produto: " + p1.nome + "foi de: " + precofinal1);
 		System.out.println("O preço do produto: " + p2.nome + "foi de: " + precofinal2);
