a77b94f2bbd0dc7289c5c872f42f777a93ae04eb
Gabriel Siqueira Gama <siqueiragamagabriel@gmail.com>
2023-11-04 09:15:23 -0300

    Produto e estatico

--- retorno/src/orientacaoObjeto/Produto.java
+++ retorno/src/orientacaoObjeto/Produto.java
@@ -3,8 +3,13 @@ package orientacaoObjeto;
 public class Produto {
 	String nome;
 	double preco;
-	double desconto;
+	static double desconto;
 	
+	Produto(){}
+	Produto(String nomeInicial, double precoInicial){
+		nome = nomeInicial;
+		preco = precoInicial;
+	}
 	double precoComDesconto(double desconto) {
 		return preco * (1 - (desconto/100));
 	}
--- retorno/src/orientacaoObjeto/TesteProduto.java
+++ retorno/src/orientacaoObjeto/TesteProduto.java
@@ -6,15 +6,14 @@ public class TesteProduto {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		Produto p1 = new Produto();
+		Produto.desconto = 30;
 		p1.nome = "Notebook";
 		p1.preco = 120.50;
-		p1.desconto = 25;
 		
 		var p2 = new Produto();
 		p2.nome = "Caneta azul";
 		p2.preco = 100;
 		System.out.println("Digite o valor do desconto");
-		p2.desconto = scn.nextDouble();
 		
 		double precofinal1 = p1.preco * (1 -(p1.desconto/100));
 		double precofinal2 = p2.precoComDesconto(p2.desconto);
