2782a37a918ff71e0c037b02db878df1ee5ff7c1
Gabriel Siqueira Gama <siqueiragamagabriel@gmail.com>
2023-11-04 09:36:56 -0300

    This() e this.

--- retorno/src/orientacaoObjeto/Dataa.java
+++ retorno/src/orientacaoObjeto/Dataa.java
@@ -5,6 +5,14 @@ public class Dataa {
 	int mes;
 	int ano;
 	
+	Dataa(){
+		this(01,11, 1970);
+	}
+	Dataa(int dia, int mes, int ano){
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+	}
 	String obterData(int dia, int mes, int ano) {
 		return String.format("%d/%d/%d", dia, mes, ano);
 	}
