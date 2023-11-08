bb6bd25a8ebc3cf39195d4487ad081042799b3ce
Gabriel Siqueira Gama <siqueiragamagabriel@gmail.com>
2023-11-08 12:15:46 -0300

    Orientação a objetos e array.

---
+++ retorno/src/array/NotasArray.java
@@ -0,0 +1,18 @@
+package array;
+
+import java.util.Arrays;
+
+public class NotasArray {
+	public static void main(String[] args) {
+		double notasAluno [] = new double[3];
+		notasAluno[0] = 6.0;
+		notasAluno[1] = 7.0;
+		notasAluno[2] = 5.0;
+		System.out.println(Arrays.toString(notasAluno));
+		double total = 0;
+		for(int i =0; i < notasAluno.length; i++ ) {
+			total += notasAluno[i];
+		}
+		System.out.println(total/3);
+	}
+}
