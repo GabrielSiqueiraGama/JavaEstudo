a26b77b02e9f720f613bac018392066700dcfe33
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2023-11-08 23:09:06 -0300

    tst

--- retorno/src/array/NotasArray.java
+++ retorno/src/array/NotasArray.java
@@ -4,11 +4,10 @@ import java.util.Arrays;
 
 public class NotasArray {
 	public static void main(String[] args) {
-		double notasAluno [] = new double[4];
+		double notasAluno [] = new double[3];
 		notasAluno[0] = 6.0;
 		notasAluno[1] = 7.0;
 		notasAluno[2] = 5.0;
-		notasAluno[3] = 5.0;
 		System.out.println(Arrays.toString(notasAluno));
 		double total = 0;
 		for(int i =0; i < notasAluno.length; i++ ) {
