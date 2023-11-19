88677dba2fa40303c315fc99bcf747d0a66bf612
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2023-11-19 18:05:42 -0300

    map

---
+++ retorno/src/colections/Mapa.java
@@ -0,0 +1,13 @@
+package colections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Mapa {
+	public static void main(String[] args) {
+		Map<Integer, String> usuarios = new HashMap<>();
+		usuarios.put(1, "bastiao");//cria usuario;
+		usuarios.put(1, "zézé");//Atualiza
+		
+	}
+}
