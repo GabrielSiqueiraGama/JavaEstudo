b217111592ddf0f2cda29287374dcd3527063356
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2023-11-22 12:05:08 -0300

    Criando classes, orientação a objetos.

---
+++ retorno/src/oo/Carro.java
@@ -0,0 +1,23 @@
+package oo;
+
+public class Carro {
+	Motor motor = new Motor();
+	void acelerar() {
+		motor.fatorInjecao += 0.4;
+	}
+	void frear() {
+		if(motor.fatorInjecao > 0.5) {
+			motor.fatorInjecao -= 0.4;
+		}
+		
+	}
+	void ligar() {
+		motor.ligado = true;
+	}
+	void desligar() {
+		motor.ligado = false;
+	}
+	boolean estaLigado(){
+		return motor.ligado;
+	}
+}
