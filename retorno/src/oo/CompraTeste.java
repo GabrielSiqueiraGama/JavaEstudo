6613db583b8727956bfaedcc483ab029dc6ba6c4
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2023-11-23 12:06:13 -0300

    Orientação objetos, um para muitos.

---
+++ retorno/src/oo/CompraTeste.java
@@ -0,0 +1,17 @@
+package oo;
+
+public class CompraTeste {
+	public static void main(String[] args) {
+		Compra c1 = new Compra();
+		c1.cliente = "zhant";
+		
+		c1.adicionarItem("Caneta azul", 20, 10);
+		c1.adicionarItem(new Item("Lapis", 1, 1.0));
+		c1.adicionarItem(new Item("Azul caneta", 20, 10));
+		
+		System.out.println(c1.itens.size());
+		System.out.println(c1.obterValorTotal());
+	}
+	
+	
+}
