ed027704458bba12d122ded8bb08cf96c0a98b2a
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-01-03 12:01:18 -0300

    Estudo de Herança, basico.

--- retorno/src/oo/heranca/Jogador.java
+++ retorno/src/oo/heranca/Jogador.java
@@ -1,8 +1,24 @@
 package oo.heranca;
 
 public class Jogador {
+	int vida = 100;
 	int x;
 	int y;
+	
+	boolean atacar(Jogador p2) {
+		int deltaX = Math.abs(x - p2.x);//Calcula a distancia entre os jogadores
+		int deltaY = Math.abs(y - p2.y);//Caso o numero seja negativo, arredonda p/1
+	if(deltaX == 0 && deltaY == 1) {
+		p2.vida -= 10;
+		return true;
+	}else if(deltaX == 1 && deltaY == 0) {
+		p2.vida -= 10;
+		return true;
+	}else {
+		return false;
+	}
+	}
+	
 	
 	Boolean andar(Direcao direcao) {
 		switch(direcao) {
--- retorno/src/oo/heranca/Jogo.java
+++ retorno/src/oo/heranca/Jogo.java
@@ -2,7 +2,22 @@ package oo.heranca;
 
 public class Jogo {
 	public static void main(String[] args) {
-		Jogador p1 = new Jogador();
-		p1.andar(Direcao.NORTE);
+		Jogador p1 = new Heroi();
+		p1.x = 10;
+		p1.y = 10;
+		
+		Jogador p2 = new Monstro();
+		p2.x = 10;
+		p2.y = 11;
+		
+		System.out.println(p1.vida);
+		System.out.println(p2.vida);
+		
+		p1.atacar(p2);
+		p2.atacar(p1);
+		p2.atacar(p1);
+		
+		System.out.println(p1.vida);
+		System.out.println(p2.vida);
 	}
 }
