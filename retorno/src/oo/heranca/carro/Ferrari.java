e6896dad0ea3b4083db486e78322cc2691e4ca66
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-01-05 22:55:18 -0300

    Aprimoramento de codigo.

--- retorno/src/oo/heranca/carro/Ferrari.java
+++ retorno/src/oo/heranca/carro/Ferrari.java
@@ -1,9 +1,8 @@
 package oo.heranca.carro;
 
 public class Ferrari extends Carro{
-	boolean acelerar() {
-		boolean acelerar1 = super.acelerar();
-		boolean acelerar2 = super.acelerar();
-		return acelerar1 || acelerar2;
+	@Override
+	void acelerar() {
+		velocidade += 10;
 	}
 }
--- retorno/src/oo/heranca/carro/Velocidades.java
+++ retorno/src/oo/heranca/carro/Velocidades.java
@@ -4,21 +4,18 @@ public class Velocidades {
 	public static void main(String[] args) {
 		Carro ferrari = new Ferrari();
 		ferrari.velocidade = 0;
+		ferrari.acelerar();//Como a função herda uma função adaptada, o valor da variavel é diferente no momento em que a chamada é realizada
 		ferrari.acelerar();
-		ferrari.acelerar();
-		System.out.println(ferrari.velocidade);
+		System.out.println("Ferrari: " + ferrari.velocidade);
 		ferrari.frear();
-		System.out.println(ferrari.velocidade);
+		System.out.println("Ferrari: " + ferrari.velocidade);
 		
 		Carro fusca = new Fusca();
 		fusca.velocidade = 10;
 		fusca.acelerar();
-		fusca.acelerar();
-		fusca.frear();
-		fusca.frear();
-		fusca.frear();
-		fusca.frear();
+		fusca.acelerar();//Aqui a variavel adiciona apenas +5 em cada vez que é chamada
+
 		
-		System.out.println(fusca.velocidade);
+		System.out.println("Fusca: " + fusca.velocidade);
 	}
 }
