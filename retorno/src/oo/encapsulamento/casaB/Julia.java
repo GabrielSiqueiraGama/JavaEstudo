7f8eaedfd8659bfe29927547e280c7bc1b9d6297
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-01-09 12:08:09 -0300

    Modificadores de acesso, sem relação.

--- retorno/src/oo/encapsulamento/casaB/Julia.java
+++ retorno/src/oo/encapsulamento/casaB/Julia.java
@@ -1,5 +1,16 @@
 package oo.encapsulamento.casaB;
 
-public class Julia {
+import oo.encapsulamento.casaA.Ana;
 
+public class Julia {
+	
+	Ana sogra = new Ana();
+	
+	void testeAcessos() {
+		//System.out.println(sogra.segredo); Não tem como acessar por ser privado
+		//System.out.println(sogra.dentroDeCasa);Não é possivel acessar por ser de pasta diferente.
+		//System.out.println(sogra.sotaque);Não é possivel acessar por ser de pasta diferente e nao herda
+		System.out.println(sogra.nome);//Publico é visto por todos.
+
+	}
 }
