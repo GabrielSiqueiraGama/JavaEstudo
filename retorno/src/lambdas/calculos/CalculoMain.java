e4cb4656470c1d53296fddb75d4ef9af9835b864
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-01-14 21:58:15 -0300

    Metodo em unica linha.

--- retorno/src/lambdas/calculos/CalculoMain.java
+++ retorno/src/lambdas/calculos/CalculoMain.java
@@ -4,7 +4,9 @@ public class CalculoMain {
 	
 	public static void main(String[] args) {
 		
-		Calculo soma = (a, b) ->{return a + b; };
-		System.out.println(soma.executar(1, 2));
+		Calculo calculo = (a, b) ->{return a + b; };
+		System.out.println(calculo.executar(1, 2));
+		
+		calculo = (a,b) -> a * b;//Caso seja uma unica função a chave se torna desnecessaria.
 	}
 }
