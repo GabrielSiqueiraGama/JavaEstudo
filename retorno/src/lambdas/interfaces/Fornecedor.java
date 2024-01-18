837cc2f1c6bbd2f776257299058b05bb3859552f
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-01-17 22:07:15 -0300

    Estudo de interface com lambda: Supplier.

---
+++ retorno/src/lambdas/interfaces/Fornecedor.java
@@ -0,0 +1,18 @@
+package lambdas.interfaces;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class Fornecedor {
+
+	public static void main(String[] args) {
+		
+		Supplier<List<String>> umaLista = () -> Arrays
+				.asList("Zhant", "Blake");
+		
+		System.out.println(umaLista.get());
+		
+	}
+	
+}
