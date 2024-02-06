e74bfd834cd3ac1539eb649dd70605964557ac8f
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-02-05 22:15:53 -0300

    Estudo de exceção checada e não checada

---
+++ retorno/src/tratamentoErros/CheckVsNot.java
@@ -0,0 +1,31 @@
+package tratamentoErros;
+
+public class CheckVsNot {
+
+	public static void main(String[] args) {
+		
+		try {
+			gerarErro1();
+		} catch (RuntimeException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		try {
+			gerarErro2();
+		} catch (Exception e) {
+			System.out.println("Ocorreu o seguinte error: " + e.getMessage());
+		}
+		
+		System.out.println("Fim!");
+	}
+	
+	//Exceção NÃO checada
+	static void gerarErro1() {
+		throw new RuntimeException("Erro de Runtime");
+	}
+	//Exceção checkada. 
+	static void gerarErro2() throws Exception{
+		throw new Exception("Erro");//throw serve para lançar o erro.
+	}
+	
+}
