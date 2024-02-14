62418726a961e93e6b5ff837f9637f304008db9d
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-02-14 12:19:50 -0300

    Estudo de Generics, exemplo sem Generics

---
+++ retorno/src/genericss/CaixaObjeto.java
@@ -0,0 +1,14 @@
+package genericss;
+
+public class CaixaObjeto {
+
+	private Object coisa;
+	
+	public void guardar(Object coisa) {
+		this.coisa = coisa;
+	}
+	public Object abrir() {
+		return coisa;
+	}
+	
+}
---
+++ retorno/src/genericss/CaixaObjetoTeste.java
@@ -0,0 +1,15 @@
+package genericss;
+
+public class CaixaObjetoTeste {
+
+	public static void main(String[] args) {
+		
+		CaixaObjeto caixaA = new CaixaObjeto();
+		caixaA.guardar(2.3);
+		
+		Double coisa = (Double) caixaA.abrir();
+		System.out.println(coisa);
+		
+	}
+	
+}
