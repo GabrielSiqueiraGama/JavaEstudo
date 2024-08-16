f1ec8f59a99bc506d1310bed0358eba84c02294c
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-08-16 15:26:44 -0300

    feat: :seedling: adding on array

---
+++ retorno/src/estruturaDados/VetorAula.java
@@ -0,0 +1,13 @@
+package estruturaDados;
+
+public class VetorAula {
+
+	public static void main(String[] args) {
+		Vetores vetor = new Vetores(3);
+		
+		vetor.adicionar(2);
+		vetor.adicionar(3);
+		vetor.adicionar(43);
+		
+	}
+}
--- retorno/src/estruturaDados/Vetores.java
+++ retorno/src/estruturaDados/Vetores.java
@@ -1,20 +1,17 @@
 package estruturaDados;
 
 public class Vetores {
+	double [] temperaturas;
 
-	public static void main(String[] args) {
-		double [] temperaturas = new double[365];
-		
-		temperaturas[0] = 12;
-		temperaturas[1] = 18;
-		temperaturas[2] = 34;
-		temperaturas[3] = 22;
-		
-		System.out.println("O item na posição inicial tem como valor " + temperaturas[0]);
-		System.out.println("O tamanho da lista é de: " + temperaturas.length);
-		
-		for(int i =0; i<temperaturas.length; i++) {
-			System.out.println("O item na posição " + i + " tem como valor: " + temperaturas[i]);
+	public Vetores(int capacidade) {
+		this.temperaturas = new double[capacidade];
+	}
+	
+	public void adicionar(double temperatura) {
+		for(int i = 0; i <this.temperaturas.length; i++) {
+			if(this.temperaturas[i] == 0) {
+				this.temperaturas[i] = temperatura;
+			}
 		}
 	}
 }
